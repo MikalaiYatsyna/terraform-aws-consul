1a34abd2ceae59c6a8dc17afa42f1b973e8fd710
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 20:33:58 +0100

    k8s provider
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,29 @@
 terraform {
-  required_version = "1.7.0"
+  required_version = "1.7.5"
   required_providers {
     aws = {
-      version = "5.40.0"
+      version = "4.59.0"
       source  = "hashicorp/aws"
+    }
+    kubernetes = {
+      version = "2.18.1"
+      source = "hashicorp/kubernetes"
+    }
+    helm = {
+      version = "2.9.0"
+      source = "hashicorp/helm"
+    }
+    vault = {
+      version = "3.14.0"
+      source = "hashicorp/vault"
+    }
+    consul = {
+      version = "2.17.0"
+      source = "hashicorp/consul"
+    }
+    random = {
+      version = "3.4.3"
+      source = "hashicorp/random"
     }
   }
 }
