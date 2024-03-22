1dd62167242c56b9cc57f058f5fcf93447a7fee1
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-22 21:12:30 +0100

    Refactor kubernetes authentication
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- data.tf
+++ data.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "eks_cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks_cluster_auth" {
-  name = var.cluster_name
-}
-
 data "aws_secretsmanager_secret_version" "root_token" {
   secret_id = var.vault_token_secret_id
 }
--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "stack" {
   description = "Stack name"
 }
 
-variable "cluster_name" {
-  type        = string
-  description = "Name of EKS cluster"
-}
-
 variable "namespace" {
   type        = string
   description = "Namespace for Consul release"
@@ -65,4 +60,26 @@ variable "pki_backend" {
 variable "kv_backend" {
   type        = string
   description = "PKI backend path"
+}
+
+variable "cluster_endpoint" {
+  sensitive   = true
+  type        = string
+  description = "Endpoint of the cluster."
+}
+
+variable "cluster_ca" {
+  sensitive   = true
+  type        = string
+  description = "CA certificate of the cluster."
+}
+
+variable "k8s_exec_args" {
+  type        = list(string)
+  description = "Args for Kubernetes provider exec plugin. Example command ['eks', 'get-token', '--cluster-name', '{clusterName}}']"
+}
+
+variable "k8s_exec_command" {
+  type        = string
+  description = "Command name for Kubernetes provider exec plugin. Example - 'aws"
 }
